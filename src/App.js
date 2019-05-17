3ea850ff4e7d07f97b1dbcb1034f77271b0a7550
Adrian Wilcock <adrian@Adrians-MacBook-Pro.local>
2019-05-17 22:53:32 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import TypoGraphy from '@material-ui/core/Typography';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import {
+  FormControl,
+  InputLabel,
+  Input,
+  TextField,
+} from "@material-ui/core";
 
-function App() {
+
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div>
+    <div>
+    <AppBar color="primary" position="static">
+    <Toolbar>
+      <TypoGraphy variant="title" color="inherit">Stupid Form</TypoGraphy>
+    </Toolbar>
+      
+    </AppBar>
+  </div>
+
+<div>
+<div style={{ display: "flex", justifyContent: "center", margin: 20, padding: 20 }}>
+        <form style={{ width: "50%" }}>
+          <h1>Contact Form</h1>
+
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="name">First Name</InputLabel>
+            <Input id="name" type="text" />
+          </FormControl>
+
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="name">Second Name</InputLabel>
+            <Input id="name" type="text" />
+          </FormControl>
+
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="name">Username</InputLabel>
+            <Input id="name" type="text" />
+          </FormControl>
+
+          <TextField id="standard-password-input" label="Password" type="password" autoComplete="current-password" margin="normal"/>
+
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input id="email" type="email" />
+          </FormControl>
+
+          <FormControl margin="normal" fullWidth>
+            <InputLabel htmlFor="phonenumber">Phone Number</InputLabel>
+            <Input id="phonenumber" type="phonenumber" />
+          </FormControl>
+
+        <FormControl>
+          <Checkbox label="Gender"/>
+        </FormControl>
+
+          <Button variant="contained" color="primary" size="medium">
+            Send
+          </Button>
+
+        </form>
+      </div>
+    );
+  }
+}
+</div>
+
     </div>
   );
 }
+}
+
 
 export default App;
