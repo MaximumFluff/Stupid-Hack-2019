6025c7eb7c7b74eea331648bc7be70c7a50ee779
Alex Jacobs <alexander.jacobs@industry62.fi>
2019-05-18 09:54:16 +0300

    stuff

--- src/components/Forgot.js
+++ src/components/Forgot.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer, toast, cssTransition } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+const Zoom = cssTransition({
+  //enter: 'bounceIn',
+  //exit: 'bounceOut',
+  duration: [10000000, 10000000]
+});
 
 export default class Forgot extends Component {
   notify = () => {
     toast.warn("Try to remember your password :-)", {
-      position: toast.POSITION.TOP_CENTER
+      position: toast.POSITION.TOP_CENTER,
+      //transition: Zoom,
     });
   };
 
